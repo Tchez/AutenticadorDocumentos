584a676eb4a69643f056cc87f2f0790f796999db
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-09-01 16:01:25 -0300

    feat: :sparkles: adicionando criação de chaves

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from . import views
 
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+urlpatterns = [    
+    path("generate_keys/", views.generate_keys, name="generate_keys"),
+    path("show_my_keys/", views.show_my_keys, name="show_my_keys"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .crypto_utils import generate_keypair
 
-# Create your views here.
+
+@login_required
+def generate_keys(request):
+    if request.method == "POST":
+        private_key, public_key = generate_keypair()
+        request.user.private_key = private_key
+        request.user.public_key = public_key
+        request.user.save()
+        return redirect("show_my_keys")
+    return render(request, "generate_keys.html")
+
+
+@login_required
+def show_my_keys(request):
+    public_key = request.user.public_key
+    private_key = request.user.private_key
+
+    context = {
+        "public_key": public_key,
+        "private_key": private_key,
+    }
+
+    return render(request, "show_my_keys.html", context)
