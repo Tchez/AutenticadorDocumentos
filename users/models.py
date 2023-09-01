7b580c98c9d93b4700cd418d7e6210c769c0077c
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-09-01 15:40:48 -0300

    feat: :sparkles: first commit

---
+++ users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    private_key = models.TextField(blank=True, null=True)
+    public_key = models.TextField(blank=True, null=True)
